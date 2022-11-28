013189a60ee2ed077c35819eebd79ca37146cb47
Sebastian Padilla <70664928+SebastianPadillaZabala@users.noreply.github.com>
2022-11-28 18:58:28 -0400

    nuevo2

--- app.js
+++ app.js
@@ -22,6 +22,14 @@ dotenv.config({path: './env/.env'})
 //para poder trabajar con las cookies
 app.use(cookieParser())
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 //llamar al router
 app.use('/', require('./routes/router.js'))
 
