7ca0e26de38f1fd79814bb00d5a7486853bdc63f
Sebastian Padilla <70664928+SebastianPadillaZabala@users.noreply.github.com>
2022-11-28 19:59:14 -0400

    nuevo7

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser())
 app.use('/', require('./routes/router.js'))
 
 var server = app.listen(process.env.PORT || 8080, ()=>{
-    console.log('SERVER UP running in https://ec2-52-87-253-204.compute-1.amazonaws.com/:8080')
+    console.log('SERVER UP running in https://ec2-54-174-30-16.compute-1.amazonaws.com:8080')
 })
 
 const io = socket(server)
